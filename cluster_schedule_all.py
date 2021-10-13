5db1510f43a180f67a7a95bfbc8dcecc89f645c0
hvpham <hv.pham.2704@gmail.com>
2021-10-13 13:33:38 -0400

    Include new experiment runs

--- cluster_schedule_all.py
+++ cluster_schedule_all.py
@@ -172,8 +172,31 @@ def list_cifar100_holdout_val_test_runs(local_result_folder, no_runs):
     #holdout_class_list = ['caterpillar', 'mushroom', 'porcupine', 'ray']
 
     #holdout_class_list = ['turtle', 'shark', 'crocodile', 'caterpillar', 'possum', 'squirrel', 'ray', 'shrew', 'lizard', 'beaver', 'rabbit', 'seal']
-    holdout_class_list = ['boy', 'maple_tree', 'oak_tree', 'willow_tree', 'man', 'woman', 'pine_tree',
-                          'apple', 'girl', 'orange', 'rose', 'cockroach', 'tulip', 'baby', 'palm_tree', 'poppy', 'pear', 'whale']
+    # holdout_class_list = ['boy', 'maple_tree', 'oak_tree', 'willow_tree', 'man', 'woman', 'pine_tree',
+    #                      'apple', 'girl', 'orange', 'rose', 'cockroach', 'tulip', 'baby', 'palm_tree', 'poppy', 'pear', 'whale']
+
+    aquatic_mammals_list = ['beaver', 'dolphin', 'otter', 'seal', 'whale']  # 0
+    fish_list = ['aquarium_fish', 'flatfish', 'ray', 'shark', 'trout']  # 1
+    flower_list = ['orchid', 'poppy', 'rose', 'sunflower', 'tulip']  # 2
+    fruit_and_vegetables_list = ['apple', 'mushroom', 'orange', 'pear', 'sweet_pepper']  # 3
+    insects_list = ['bee', 'beetle', 'butterfly', 'caterpillar', 'cockroach']  # 4
+    medium_mammals_list = ['fox', 'porcupine', 'possum', 'raccoon', 'skunk']  # 5
+    people_list = ['baby', 'boy', 'girl', 'man', 'woman']  # 6
+    reptiles_list = ['crocodile', 'dinosaur', 'lizard', 'snake', 'turtle']  # 7
+    small_mammals_list = ['hamster', 'mouse', 'rabbit', 'shrew', 'squirrel']  # 8
+    trees_list = ['maple_tree', 'oak_tree', 'palm_tree', 'pine_tree', 'willow_tree']  # 9
+
+    holdout_class_list = []
+    holdout_class_list.extend(aquatic_mammals_list)  # 0
+    holdout_class_list.extend(fish_list)  # 1
+    holdout_class_list.extend(flower_list)  # 2
+    holdout_class_list.extend(fruit_and_vegetables_list)  # 3
+    holdout_class_list.extend(insects_list)  # 4
+    holdout_class_list.extend(medium_mammals_list)  # 5
+    holdout_class_list.extend(people_list)  # 6
+    holdout_class_list.extend(reptiles_list)  # 7
+    holdout_class_list.extend(small_mammals_list)  # 8
+    holdout_class_list.extend(trees_list)  # 9
 
     runs_list = []
     for holdout_class in holdout_class_list:
@@ -182,7 +205,7 @@ def list_cifar100_holdout_val_test_runs(local_result_folder, no_runs):
         for ratio in range(11):
             # for ratio in [10]:
             for run_id in range(no_runs):
-                outputs_file = os.path.join(local_result_folder, 'cifar100', 'cifar100-%s_%s_%d' % (mode, holdout_class, ratio), 'outputs_var_%s' % run_id)
+                outputs_file = os.path.join(local_result_folder, 'cifar100', 'cifa-r100-%s_%s_%d' % (mode, holdout_class, ratio), 'outputs_var_%s' % run_id)
                 if not check_done(outputs_file):
                     script = "cluster_single_val_test_cifar100.py"
                     args = "%s %s %d %d" % (mode, holdout_class, ratio, run_id)
@@ -196,12 +219,35 @@ def list_cifar100_holdout_val_train_more_runs(local_result_folder, no_runs):
     mode = 'holdout'
 
     #holdout_class_list = ['turtle', 'shark', 'crocodile', 'caterpillar', 'possum', 'squirrel', 'ray', 'shrew', 'lizard', 'beaver', 'rabbit', 'seal']
-    holdout_class_list = ['boy', 'maple_tree', 'oak_tree', 'willow_tree', 'man', 'woman', 'pine_tree',
-                          'apple', 'girl', 'orange', 'rose', 'cockroach', 'tulip', 'baby', 'palm_tree', 'poppy', 'pear', 'whale']
+    # holdout_class_list = ['boy', 'maple_tree', 'oak_tree', 'willow_tree', 'man', 'woman', 'pine_tree',
+    #                      'apple', 'girl', 'orange', 'rose', 'cockroach', 'tulip', 'baby', 'palm_tree', 'poppy', 'pear', 'whale']
+
+    aquatic_mammals_list = ['beaver', 'dolphin', 'otter', 'seal', 'whale']  # 0
+    fish_list = ['aquarium_fish', 'flatfish', 'ray', 'shark', 'trout']  # 1
+    flower_list = ['orchid', 'poppy', 'rose', 'sunflower', 'tulip']  # 2
+    fruit_and_vegetables_list = ['apple', 'mushroom', 'orange', 'pear', 'sweet_pepper']  # 3
+    insects_list = ['bee', 'beetle', 'butterfly', 'caterpillar', 'cockroach']  # 4
+    medium_mammals_list = ['fox', 'porcupine', 'possum', 'raccoon', 'skunk']  # 5
+    people_list = ['baby', 'boy', 'girl', 'man', 'woman']  # 6
+    reptiles_list = ['crocodile', 'dinosaur', 'lizard', 'snake', 'turtle']  # 7
+    small_mammals_list = ['hamster', 'mouse', 'rabbit', 'shrew', 'squirrel']  # 8
+    trees_list = ['maple_tree', 'oak_tree', 'palm_tree', 'pine_tree', 'willow_tree']  # 9
+
+    holdout_class_list = []
+    holdout_class_list.extend(aquatic_mammals_list)  # 0
+    holdout_class_list.extend(fish_list)  # 1
+    holdout_class_list.extend(flower_list)  # 2
+    holdout_class_list.extend(fruit_and_vegetables_list)  # 3
+    holdout_class_list.extend(insects_list)  # 4
+    holdout_class_list.extend(medium_mammals_list)  # 5
+    holdout_class_list.extend(people_list)  # 6
+    holdout_class_list.extend(reptiles_list)  # 7
+    holdout_class_list.extend(small_mammals_list)  # 8
+    holdout_class_list.extend(trees_list)  # 9
 
     runs_list = []
     for holdout_class in holdout_class_list:
-        for retrain_mode in ['random', 'std_conf', 'avg_conf', 'conf_worst', 'conf_best', 'conf_median']:
+        for retrain_mode in ['random', 'std_conf', 'avg_conf', 'single_conf']:
             # for retrain_mode in ['random']:
             # for ratio in [0,3,6,9]:
             # for ratio in [1,2,4,5,7,8,10]:
@@ -304,6 +350,7 @@ def list_cifar10_runs(local_result_folder, no_runs):
 
 parser = argparse.ArgumentParser(description='Schedule experiment runs on the clusters with holdout CIFAR-100 and Resnet18')
 parser.add_argument('local_teamdrive_folder', help='local teamdrive folder')
+parser.add_argument('job', choices=['val_test', 'retrain'], help='which job to run')
 
 args = parser.parse_args()
 
@@ -311,13 +358,14 @@ local_teamdrive_folder = args.local_teamdrive_folder
 
 NO_RUNS = 25
 
+if args.job == 'val_test':
+    runs_list = list_cifar100_holdout_val_test_runs(os.path.join(local_teamdrive_folder, 'result'), NO_RUNS)
+elif args.job == 'retrain':
+    runs_list = list_cifar100_holdout_val_train_more_runs(os.path.join(local_teamdrive_folder, 'result'), NO_RUNS)
+
 #runs_list = list_cifar10_runs(os.path.join(local_teamdrive_folder, 'result'), NO_RUNS)
 
 #runs_list = list_cifar100_holdout_runs(os.path.join(local_teamdrive_folder, 'result'), NO_RUNS)
-
-#runs_list = list_cifar100_holdout_val_test_runs(os.path.join(local_teamdrive_folder, 'result'), NO_RUNS)
-
-runs_list = list_cifar100_holdout_val_train_more_runs(os.path.join(local_teamdrive_folder, 'result'), NO_RUNS)
 
 #runs_list = list_cifar100_holdout_evaluate_runs(NO_RUNS)
 
---
+++ cluster_schedule_all_val_test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+source /anaconda/etc/profile.d/conda.sh
+
+conda activate azureml_py36_pytorch
+
+python cluster_schedule_all.py ~/teamdrive/mlvariance val_test
+
+conda deactivate
