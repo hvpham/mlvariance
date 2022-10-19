75ea52f8e769a8185940762b570a5afe3bcd5c03
hvpham <hv.pham.2704@gmail.com>
2022-10-19 15:06:06 -0400

    Update cluster_schedule_all_train.sh

--- cluster_schedule_all_train.sh
+++ cluster_schedule_all_train.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source /anaconda/etc/profile.d/conda.sh
 
-conda activate azureml_py36_pytorch
+conda activate azureml_py38_PT_and_TF
 
 python cluster_schedule_all.py ~/teamdrive/mlvariance train
 
